f65750de619c219cce8bff77293740f7bc1ac2d9
astonuser <grisdfsd@sdf.rt>
2025-01-14 18:48:30 +0300

    добавил коммит2

---
+++ Main1.java
@@ -0,0 +1,50 @@
+public class Main1 {
+
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 5;  // Пример значений
+        int b = -3; // Пример значений
+        int sum = a + b;
+
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 150; // Пример значения
+
+        if (value < 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 10; // Пример значений
+        int b = 5;  // Пример значений
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
